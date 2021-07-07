d327cdb8c3daa34b4ba28dc5c5a675e3bdabe1df
Camila Donda <camiladonda@192-168-1-108.tpgi.com.au>
2021-07-07 20:24:11 +1000

    First Commit

---
+++ etapas.js
@@ -0,0 +1,26 @@
+let etapas = [
+    {
+    titulo: 'PRESIDENT',
+    numeros: 5,
+    candidatos:[
+        {
+        numero: '38111',
+        nome: 'Homer Simpson',
+        partido: 'Homer Town',
+        fotos: [
+            {url: 'prefeito.jpeg', legenda: 'President - One Nation PARTY'}
+        ]
+    
+},
+    {
+            numero: '77222',
+        nome: 'Michael Jackson',
+        partido: 'Michael Town',
+        fotos: [
+            {url: 'vice.jpeg', legenda: 'President - LABOUR PARTY', small: true}
+        ]
+    
+},
+    ]}
+];
+
